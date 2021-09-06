8f4119549b02a618ac1f6dcf489acf2b2518c2e9
jhyea0729 <jhyea0729@gmail.com>
2021-09-06 10:59:49 +0900

    환경설정

---
+++ src/main/java/com/cos/yjh/YjhApplication.java
@@ -0,0 +1,13 @@
+package com.cos.yjh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YjhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YjhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cos/yjh/YjhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.yjh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YjhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
